30a82a99cb947da6770549e0aa6de8daefbe7c61
Michael Anyi <mikeyeucresta@gmail.com>
2024-01-03 00:59:11 +0100

    more configurations

--- src/child.rs
+++ src/child.rs
@@ -13,6 +13,12 @@ const STACK_SIZE: usize = 1024 * 1024;
 pub fn generate_child_process(config: ContainerOpts) -> Result<Pid, Errcode> {
     let mut tmp_stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
     let mut flags = CloneFlags::empty();
+    flags.insert(CloneFlags::CLONE_NEWNS);
+    flags.insert(CloneFlags::CLONE_NEWCGROUP);
+    flags.insert(CloneFlags::CLONE_NEWPID);
+    flags.insert(CloneFlags::CLONE_NEWIPC);
+    flags.insert(CloneFlags::CLONE_NEWNET);
+    flags.insert(CloneFlags::CLONE_NEWUTS);
 
     //Flags definition
     match clone(Box::new(|| child(config.clone())), &mut tmp_stack, flags, Some(Signal::SIGCHLD as i32)) {
--- src/container.rs
+++ src/container.rs
@@ -1,26 +1,33 @@
 use std::os::fd::RawFd;
 
-use crate::cli::Args;
-use crate::error::Errcode;
-use crate::config::ContainerOpts;
+use crate::{
+    cli::Args,
+    error::Errcode,
+    config::ContainerOpts,
+    child::generate_child_process
+};
 
 use nix::{
     sys::utsname::uname,
-    unistd::close
+    sys::wait::waitpid,
+    unistd::{close, Pid}
 };
 
 pub struct Container {
     sockets: (RawFd, RawFd),
-    config: ContainerOpts
+    config: ContainerOpts,
+    child_pid: Option<Pid>
 }
 
 impl Container {
     pub fn new(args: Args) -> Result<Container, Errcode>  {
         let (config, sockets) = ContainerOpts::new(args.command, args.uid, args.mount_dir)?;
-        Ok(Container { sockets, config })
+        Ok(Container { sockets, config, child_pid: None })
     }
     
     pub fn create(&mut self) -> Result<(), Errcode> {
+        let pid = generate_child_process(self.config.clone())?;
+        self.child_pid = Some(pid);
         log::debug!("Creation finished");
         Ok(())
     }
@@ -48,7 +55,19 @@ pub fn start(args: Args) -> Result<(), Errcode> {
         return Err(e);
     }
     log::debug!("Finished cleaning & exit");
+    wait_child(container.child_pid)?;
     container.clean_exit()
+}
+
+pub fn wait_child(pid: Option<Pid>) -> Result<(), Errcode>{
+    if let Some(child_pid) = pid {
+        log::debug!("Waiting for child (pid {}) to finish", child_pid);
+        if let Err(e) = waitpid(child_pid, None){
+            log::error!("Error while waiting for pid to finish: {:?}", e);
+            return Err(Errcode::ContainerError(1));
+        }
+    }
+    Ok(())
 }
 
 pub const MINIMAL_KERNEL_VERSION: f32 = 4.8;
